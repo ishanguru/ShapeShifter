f5c83ee848f24fa7261ad3cf7ceeb5e3205598e8
RajivJT <rjt2132@columbia.edu>
2016-11-04 17:46:28 -0400

    Minor fixes for semantic analysis (still disabled).

--- semant.ml
+++ semant.ml
@@ -52,7 +52,7 @@ let check (globals, functions) =
        locals = []; body = [] } (StringMap.singleton "printb"
      { typ = Void; fname = "printb"; formals = [(Bool, "x")];
        locals = []; body = [] })
-   in
+  in
      
   let function_decls = List.fold_left (fun m fd -> StringMap.add fd.fname fd m)
                          built_in_decls functions
@@ -62,8 +62,7 @@ let check (globals, functions) =
        with Not_found -> raise (Failure ("unrecognized function " ^ s))
   in
 
-  (* This is where we'll have to make sure a "Scene" is defined*)
-  let _ = function_decl "main" in (* Ensure "main" is defined *)
+  let _ = function_decl "scene" in (* Ensure "scene" is defined *)
 
   let check_function func =
 
@@ -95,38 +94,37 @@ let check (globals, functions) =
       | BoolLit _ -> Bool
       | Id s -> type_of_identifier s
       | Binop(e1, op, e2) as e -> let t1 = expr e1 and t2 = expr e2 in
-	(match op with
-          Add | Sub | Mult | Div when t1 = Int && t2 = Int -> Int
-	| Equal | Neq when t1 = t2 -> Bool
-	| Less | Leq | Greater | Geq when t1 = Int && t2 = Int -> Bool
-	| And | Or when t1 = Bool && t2 = Bool -> Bool
-        | _ -> raise (Failure ("illegal binary operator " ^
-              string_of_typ t1 ^ " " ^ string_of_op op ^ " " ^
-              string_of_typ t2 ^ " in " ^ string_of_expr e))
-        )
+          (match op with
+              Add | Sub | Mult | Div when t1 = Int && t2 = Int -> Int
+            | Equal | Neq when t1 = t2 -> Bool
+            | Less | Leq | Greater | Geq when t1 = Int && t2 = Int -> Bool
+            | And | Or when t1 = Bool && t2 = Bool -> Bool
+            | _ -> raise (Failure ("illegal binary operator " ^
+                          string_of_typ t1 ^ " " ^ string_of_op op ^ " " ^
+                          string_of_typ t2 ^ " in " ^ string_of_expr e)))
       | Unop(op, e) as ex -> let t = expr e in
-	 (match op with
-	   Neg when t = Int -> Int
-	 | Not when t = Bool -> Bool
-         | _ -> raise (Failure ("illegal unary operator " ^ string_of_uop op ^
-	  		   string_of_typ t ^ " in " ^ string_of_expr ex)))
+          (match op with
+              Neg when t = Int -> Int
+            | Not when t = Bool -> Bool
+            | _ -> raise (Failure ("illegal unary operator " ^ string_of_uop op ^
+                          string_of_typ t ^ " in " ^ string_of_expr ex)))
       | Noexpr -> Void
       | Assign(var, e) as ex -> let lt = type_of_identifier var
                                 and rt = expr e in
-        check_assign lt rt (Failure ("illegal assignment " ^ string_of_typ lt ^
-				     " = " ^ string_of_typ rt ^ " in " ^ 
-				     string_of_expr ex))
+          check_assign lt rt (Failure ("illegal assignment " ^ string_of_typ lt ^
+				              " = " ^ string_of_typ rt ^ " in " ^ 
+				              string_of_expr ex))
       | Call(fname, actuals) as call -> let fd = function_decl fname in
-         if List.length actuals != List.length fd.formals then
-           raise (Failure ("expecting " ^ string_of_int
-             (List.length fd.formals) ^ " arguments in " ^ string_of_expr call))
-         else
-           List.iter2 (fun (ft, _) e -> let et = expr e in
-              ignore (check_assign ft et
-                (Failure ("illegal actual argument found " ^ string_of_typ et ^
-                " expected " ^ string_of_typ ft ^ " in " ^ string_of_expr e))))
-             fd.formals actuals;
-           fd.typ
+          if List.length actuals != List.length fd.formals then
+            raise (Failure ("expecting " ^ string_of_int
+              (List.length fd.formals) ^ " arguments in " ^ string_of_expr call))
+          else
+            List.iter2 (fun (ft, _) e -> let et = expr e in
+               ignore (check_assign ft et
+                 (Failure ("illegal actual argument found " ^ string_of_typ et ^
+                 " expected " ^ string_of_typ ft ^ " in " ^ string_of_expr e))))
+              fd.formals actuals;
+            fd.typ
     in
 
     let check_bool_expr e = if expr e != Bool
@@ -135,25 +133,22 @@ let check (globals, functions) =
 
     (* Verify a statement or throw an exception *)
     let rec stmt = function
-	Block sl -> let rec check_block = function
-           [Return _ as s] -> stmt s
-         | Return _ :: _ -> raise (Failure "nothing may follow a return")
-         | Block sl :: ss -> check_block (sl @ ss)
-         | s :: ss -> stmt s ; check_block ss
-         | [] -> ()
+	  | Block sl -> let rec check_block = function
+          | [Return _ as s] -> stmt s
+          | Return _ :: _ -> raise (Failure "nothing may follow a return")
+          | Block sl :: ss -> check_block (sl @ ss)
+          | s :: ss -> stmt s ; check_block ss
+          | [] -> ()
         in check_block sl
       | Expr e -> ignore (expr e)
       | Return e -> let t = expr e in if t = func.typ then () else
-         raise (Failure ("return gives " ^ string_of_typ t ^ " expected " ^
+          raise (Failure ("return gives " ^ string_of_typ t ^ " expected " ^
                          string_of_typ func.typ ^ " in " ^ string_of_expr e))
-           
       | If(p, b1, b2) -> check_bool_expr p; stmt b1; stmt b2
       | For(e1, e2, e3, st) -> ignore (expr e1); check_bool_expr e2;
                                ignore (expr e3); stmt st
       | While(p, s) -> check_bool_expr p; stmt s
     in
-
     stmt (Block func.body)
-   
   in
   List.iter check_function functions
--- shapeshifter.ml
+++ shapeshifter.ml
@@ -12,12 +12,11 @@ let _ =
   else Compile in
   let lexbuf = Lexing.from_channel stdin in
   let ast = Parser.program Scanner.token lexbuf in
-  (* TODO: uncomment this when the semantic analyzer is done *)
   (*Semant.check ast;*)
   match action with
-    Ast -> print_string (Ast.string_of_program ast)
-  | PrettyPrint -> print_string (Pp.string_of_program ast)
-  | LLVM_IR -> print_string (Llvm.string_of_llmodule (Codegen.translate ast))
-  | Compile -> let m = Codegen.translate ast in
-    Llvm_analysis.assert_valid_module m;
-    print_string (Llvm.string_of_llmodule m)
+    | Ast -> print_string (Ast.string_of_program ast)
+    | PrettyPrint -> print_string (Pp.string_of_program ast)
+    | LLVM_IR -> print_string (Llvm.string_of_llmodule (Codegen.translate ast))
+    | Compile -> let m = Codegen.translate ast in
+        Llvm_analysis.assert_valid_module m;
+        print_string (Llvm.string_of_llmodule m)
