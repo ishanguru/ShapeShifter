5aa131bb63cf74d3929a8541195467e84d6f5c20
Eszter Offertaler <eo2309@columbia.edu>
2016-11-09 00:52:54 -0500

    display almost works; indices are weird

--- graphics/display/main.cpp
+++ graphics/display/main.cpp
@@ -1,33 +1,57 @@
+#define GL_GLEXT_PROTOTYPES
+
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
 #include <GL/glext.h>
 #include <cstdio>
 #include <cmath>
-#include <algorithm> // why is min/max in here??
-
+#include <algorithm>
 
 #include "files.h"
 #include "cork.h"
 
-int windowWidth = 600; 
-int windowHeight = 500; 
 
-double winL = -100; // value at left of window 
-double winR = 100; // right
-double winB = -100;  // bottom
-double winT = 100; // top
-double winN = 0; // near
-double winF = 100; 
+void check_GL_Error(const char *file, int line) 
+{
+    GLenum err = glGetError(); 
+    while (err != GL_NO_ERROR) {
+        fprintf(stderr, "OpenGL Error %x at %s, line %d\n", err, file, line);
+        err = glGetError();    
+    }
+}
+
+// OpenGL Error checking
+#define debug
+#if defined(debug)
+#define CHECK_GL(func) func; \
+    check_GL_Error(__FILE__, __LINE__); 
+#else 
+#define CHECK_GL(func) func;
+#endif
+
+int windowWidth = 600; 
+int windowHeight = 600; 
+
+double winL = -10; // value at left of window 
+double winR = 10; // right
+double winB = -10;  // bottom
+double winT = 10; // top
+double winN = -1; // near
+double winF = 1; 
+
+CorkTriMesh shape; 
+GLuint vbo; // vertex buffer object, stores triangle vertex info
+GLuint ibo; // index buffer object, stores indices of triangles
 
 void reshape(int w, int h)
 {
-    glViewport(0, 0, w, h); 
-    windowHeight = std::max(w, 1); 
+    windowWidth = std::max(w, 1); 
     windowHeight = std::max(h, 1);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity(); 
-    glOrtho(winL, winR, winB, winT, winN, winF);
+    CHECK_GL(glViewport(0, 0, windowHeight, windowHeight)); 
+    CHECK_GL(glMatrixMode(GL_PROJECTION));
+    CHECK_GL(glLoadIdentity()); 
+    CHECK_GL(glOrtho(winL, winR, winB, winT, winN, winF));
 }
 
 void keyboard(unsigned char key, int x, int y)
@@ -36,6 +60,7 @@ void keyboard(unsigned char key, int x, int y)
         exit(0); 
     }
     // add other key functions - zoom? translate? animate?
+    // toggle axis display
 }
 
 // Upon mouse interaction
@@ -48,17 +73,70 @@ void mouse(int button, int state, int x, int y)
 // Run when not displaying... 
 void idle() 
 {
+    //glutPostRedisplay();
 
+}
 
+void uploadMeshData()
+{
+    CHECK_GL(glGenBuffers(1, &vbo)); 
+    CHECK_GL(glGenBuffers(1, &ibo)); 
+    CHECK_GL(glBindBuffer(GL_ARRAY_BUFFER, vbo)); 
+    CHECK_GL(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));    
+ 
+    CHECK_GL(glBufferData(GL_ARRAY_BUFFER, shape.n_vertices*3*sizeof(float), shape.vertices, GL_STATIC_DRAW));
+    CHECK_GL(glBufferData(GL_ELEMENT_ARRAY_BUFFER, shape.n_triangles*3*sizeof(uint), shape.triangles, GL_STATIC_DRAW)); 
+ 
+    CHECK_GL(glBindBuffer(GL_ARRAY_BUFFER, 0)); 
+    CHECK_GL(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0)); 
 }
 
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
     glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity(); 
+    //glLoadIdentity(); 
 
-    // Draw things
+    // Draw axes
+    glBegin(GL_LINES);  
+    glColor3f(1.0, 0.0, 0.0); // x 
+    glVertex3f(-100.0, 0.0, 0.0); 
+    glVertex3f(100.0, 0.0, 0.0); 
+    glColor3f(0.0, 1.0, 0.0); // y
+    glVertex3f(0.0, -100.0, 0.0); 
+    glVertex3f(0.0, 100.0, 0.0); 
+    glColor3f(0.0, 0.0, 1.0); // z
+    glVertex3f(0.0, 0.0, -100.0); 
+    glVertex3f(0.0, 0.0, 100.0); 
+    glEnd(); 
+
+    glColor3f(.2, .2, .2);
+
+    CHECK_GL(glBindBuffer(GL_ARRAY_BUFFER, vbo)); 
+    CHECK_GL(glEnableClientState(GL_VERTEX_ARRAY));
+    CHECK_GL(glVertexPointer(3, GL_FLOAT, 0, 0)); 
+
+//    CHECK_GL(glDrawArrays(GL_TRIANGLES, 0, 3*shape.n_triangles)); 
+
+    CHECK_GL(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+    CHECK_GL(glDrawElements(GL_TRIANGLES, shape.n_triangles*3, GL_UNSIGNED_INT, (void*)0));
+
+    CHECK_GL(glDisableClientState(GL_VERTEX_ARRAY));
+    CHECK_GL(glBindBuffer(GL_ARRAY_BUFFER, 0)); 
+
+
+/*
+    glBegin(GL_TRIANGLES); 
+    glColor3f(1.0, 0.0, 0.0); 
+    glVertex3f(-1.0f, -1.0f, 0.0f);
+    glColor3f(0.0, 1.0, 0.0);  
+    glVertex3f(1.0f, -1.0f, 0.0f);
+    glColor3f(0.0, 0.0, 1.0);      
+    glVertex3f(0.0f, 1.0f, 0.0f); 
+    glEnd(); 
+  */
+  
+
 
     glutSwapBuffers(); 
 
@@ -88,10 +166,9 @@ void initOpenGLandGLUT(int argc, char **argv)
     glDisable(GL_LIGHTING); 
     glDisable(GL_DEPTH_TEST); 
     // Set background color to gray
-    glClearColor(.7, .7, .7, 1.0);     
+    CHECK_GL(glClearColor(.7, .7, .7, 1.0));     
 
     reshape(windowWidth, windowHeight); 
-    glutMainLoop(); 
 
 }
 
@@ -132,8 +209,13 @@ int main(int argc, char **argv)
     }
     
     // read the thing
-    CorkTriMesh in; 
-
+ 
     initOpenGLandGLUT(argc, argv); 
+
+    loadMesh(argv[1], &shape);    
+
+    uploadMeshData(); 
+   
     glutMainLoop(); 
+   
 }
